6fe8334b4b6593e397855ca411b2cccfbde02df7
PatelDishaJ <disha.patel0608@gmail.com>
2025-07-05 15:56:31 +0530

    feat: Handle tests for inputs with custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,16 @@ function add(numbers){
     numbers=numbers.trim();// Handles "", "   ", "\n"
     if(numbers==="") 
         return 0;
-    const numList=numbers.split(/,|\n/).map(n=>n.trim()).filter(n=>n!=="");
+    let delimiter=/,|\n/;
+    if(numbers.startsWith("//"))
+    {
+        const partition=numbers.split("\n");
+        const customDelimiter=partition[0].substring(2);
+        const escapedDelimiter = customDelimiter.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');//Replaces any RegExp-special character with an escaped version so it matches literally.
+        delimiter = new RegExp(escapedDelimiter);
+        numbers=partition.slice(1).join("\n");
+    }
+    const numList=numbers.split(delimiter).map(n=>n.trim()).filter(n=>n!=="");
     return numList.reduce((sum,num)=>sum+parseInt(num),0);
 }
 
