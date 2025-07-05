069423f06ffb9546939f2c397b65f1a34c12ea76
PatelDishaJ <disha.patel0608@gmail.com>
2025-07-05 13:06:02 +0530

    test: add tests for single number

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,6 +1,6 @@
-//Test for empty string
 const {add} = require('./stringCalculator');
 
+//Test for empty string
 test('returns 0 for empty string',()=>{
     expect(add("")).toBe(0);
 });
@@ -11,4 +11,29 @@ test('returns 0 for when string contain only space',()=>{
 
 test('returns 0 for newline only',()=>{
     expect(add("\n")).toBe(0);
+});
+
+//Test for single number
+test('return number itself for single input',()=>{
+    expect(add("5")).toBe(5);
+});
+
+test('return number itself for single input with trailing space',()=>{
+    expect(add("5 ")).toBe(5);
+});
+
+test('return number itself for single input with leading space',()=>{
+    expect(add(" 5")).toBe(5);
+});
+
+test('return number itself for single input with trailing and leading space',()=>{
+    expect(add(" 5 ")).toBe(5);
+});
+
+test('return number itself for input with newline before', () => {
+  expect(add("\n5")).toBe(5);
+});
+
+test('return number itself for input with newline after', () => {
+  expect(add("5\n")).toBe(5);
 });
