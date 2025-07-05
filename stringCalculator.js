58caba9a3aa16806967eb5b30a8d8113f449cdf9
PatelDishaJ <disha.patel0608@gmail.com>
2025-07-05 19:00:52 +0530

    refactor: clean up and organize logic in stringCalculator.js

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,36 +2,48 @@ function add(numbers){
     numbers=numbers.trim();// Handles "", "   ", "\n"
     if(numbers==="") 
         return 0;
+
     let delimiter=/,|\n/;
     let customDelimiter=null;
+
+    //check for custom delimiter
     if(numbers.startsWith("//"))
     {
-        const partition=numbers.split("\n");
-        if (partition.length < 2) {
+        const [customDel,...numbers]=numbers.split("\n");
+        customDelimiter=customDel.slice(2);
+
+        if (!customDelimiter) {
             throw new Error("Invalid custom delimiter definition");
         }
-        customDelimiter=partition[0].substring(2);
-        if (customDelimiter === "") {
-            throw new Error("Invalid custom delimiter definition");
-        }
-        const escapedDelimiter = customDelimiter.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');//Replaces any RegExp-special character with an escaped version so it matches literally.
+        //Replaces any RegExp-special character with an escaped version so it matches literally.
+        const escapedDelimiter = customDelimiter.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
         delimiter = new RegExp(escapedDelimiter);
-        numbers=partition.slice(1).join("\n");
+        numbers=numbers.join("\n");
     }
-    const numList=numbers.split(delimiter).map(n=>n.trim()).filter(n=>n!=="");
+
+    //parse number parts
+    const numList=numbers
+        .split(delimiter)
+        .map(n=>n.trim())
+        .filter(n=>n!=="");
     
+    //check for negative numbers
     const negativeNumbers = numList.filter(n => parseInt(n) < 0);
-    
     if (negativeNumbers.length > 0) {
         throw new Error(`negative numbers not allowed ${negativeNumbers.join(",")}`);
     }
-     if (!customDelimiter) {
-        const invalid = numbers.split(/,|\n/).some(part => part && /[^0-9]/.test(part));
-        if (invalid) {
+    
+    //check for no custom delimeter
+    if (!customDelimiter) {
+        const invalidCharPresent = numbers
+            .split(/,|\n/)
+            .some(part => part && /[^0-9]/.test(part));
+        if (invalidCharPresent) {
             throw new Error("Unexpected delimiter");
         }
     }
     
+    //final sum
     return numList.reduce((sum,num)=>sum+parseInt(num),0);
 }
 
